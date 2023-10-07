d0d3049301447d3b011e0f0644650ed383a4cb42
ironsummit72 <ironsumit72@gmail.com>
2023-10-07 16:05:05 +0530

    added todo functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+import { useEffect,useRef,useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Todoinput from "./Components/TodoInput/Todoinput";
+import TodoItem from "./Components/Todo/TodoItem";
+import { addLocalStorageData, createLocalStorage, createTodo, getLocalStorageData } from "./apis/TodoLocalStorage";
+const LocalStorageKey = "TODODB";
 
 function App() {
+  const handleInputRef=useRef();
+const [todoItems, settodoItems]=useState(getLocalStorageData(LocalStorageKey))
+  let InputValue=undefined
+  useEffect(() => {
+    if (localStorage.getItem(LocalStorageKey) === null) {
+      createLocalStorage(LocalStorageKey);
+    }
+  },[]);
+  
+  const HandleInput=(e)=>{
+  InputValue=e.target.value
+
+  }
+  const HandleAddBtn=()=>{
+    console.log(InputValue);
+    if(InputValue!==undefined)
+    {
+      const todoData= new createTodo(InputValue,false)
+      addLocalStorageData(LocalStorageKey,todoData)
+      settodoItems(getLocalStorageData(LocalStorageKey))
+      handleInputRef.current.value=''
+   
+    }
+  }
   return (
     <>
       <Navbar />
       <div class="app_container">
-        <Todoinput />
+        <Todoinput onHandleClickAdd={HandleAddBtn} onChangeInput={HandleInput} inputRef={handleInputRef}/>
+        {
+          todoItems.map((items,index)=>{
+             return <TodoItem key={index} todoText={items.todo} line={false} onHandleChangeCheck={()=>{}} onHandleDelete={()=>{}} onHandleEdit={()=>{}}/>
+            
+          })
+        }
       </div>
     </>
   );
