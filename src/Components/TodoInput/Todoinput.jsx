3eb350a14cb9c6f8f9061d6d1947834e9807efa4
ironsummit72 <ironsumit72@gmail.com>
2023-10-07 16:07:29 +0530

    handling events here

--- src/Components/TodoInput/Todoinput.jsx
+++ src/Components/TodoInput/Todoinput.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './todoinput.css'
 
-function Todoinput() {
+function Todoinput({onHandleClickAdd,onChangeInput,inputRef}) {
   return (
     <>
     <div className="todoinput_container">
-        <input type="text" name="text" className='todoinput' id="" placeholder='Todo' />
-        <button className='add'>Add</button>
+        <input type="text" name="text" className='todoinput' id="" placeholder='Todo' onChange={onChangeInput} ref={inputRef} />
+        <button className='add' onClick={onHandleClickAdd}>Add</button>
     </div>
     </>
   )
