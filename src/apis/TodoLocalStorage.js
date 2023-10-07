2cc284087083034a2f6936da341b89e0e0acbaed
ironsummit72 <ironsumit72@gmail.com>
2023-10-07 16:09:26 +0530

    localstorage api added for storage functionality

---
+++ src/apis/TodoLocalStorage.js
@@ -0,0 +1,31 @@
+function createTodo(todo, checked) {
+  this.todo = todo;
+  this.checked = checked;
+}
+function createLocalStorage(key) {
+  localStorage.setItem(key, []);
+}
+function getLocalStorageData(key) {
+  if (localStorage.getItem(key) !== null) {
+    const localData = localStorage.getItem(key);
+
+    return JSON.parse(localData);
+  }
+}
+function addLocalStorageData(key, data) {
+  let getItems = [];
+  if (localStorage.getItem(key) === ``) {
+    getItems.push(data);
+    localStorage.setItem(key, JSON.stringify(getItems));
+  } else {
+    getItems = JSON.parse(localStorage.getItem(key));
+    getItems.push(data);
+    localStorage.setItem(key, JSON.stringify(getItems));
+  }
+}
+export {
+  createTodo,
+  createLocalStorage,
+  getLocalStorageData,
+  addLocalStorageData,
+};
